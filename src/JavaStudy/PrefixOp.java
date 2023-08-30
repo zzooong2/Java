0a88fcc5f0bba92180da6c4daa3049a725134aff
KIM HYUNJOONG <zzooong2@naver.com>
2023-08-30 14:22:02 +0900

    day2

---
+++ src/JavaStudy/PrefixOp.java
@@ -0,0 +1,9 @@
+package JavaStudy;
+public class PrefixOp {
+    public static void main(String[] args) {
+        int num = 7;
+        System.out.println(++num);
+        System.out.println(++num);
+        System.out.println(num);
+        }
+   }
