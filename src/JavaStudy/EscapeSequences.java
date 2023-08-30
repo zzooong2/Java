dd6bc8d8a752ba9c95d8e2e686917713ef2e76f8
KIM HYUNJOONG <zzooong2@naver.com>
2023-08-30 13:48:20 +0900

    day2

---
+++ src/JavaStudy/EscapeSequences.java
@@ -0,0 +1,12 @@
+package JavaStudy;
+public class EscapeSequences {
+    public static void main(String[] args) {
+	    System.out.println("AB" + '\b' + 'c');
+	    
+	    System.out.println("AB" + '\t' + 'c');
+	    
+	    System.out.println("AB" + '\n' + 'c');
+	    
+	    System.out.println("AB" + '\r' + 'c');
+	                       	        	    }
+}
---
+++ src/JavaStudy/test.java
@@ -0,0 +1,27 @@
+package JavaStudy;
+public class test {
+    public static void main(String[] args) {
+         	System.out.println(3147483647l + 3147483648l);
+        	
+        	byte num1 = 5;
+        	short num2 = 25;
+        	System.out.println(num1);
+        	System.out.println(num2);
+        	
+        	byte seven = 0B111;
+        	int num205 = 0B11001101;
+           	System.out.println(seven);
+        	System.out.println(num205);
+        	
+        	int num = 100_000_000;
+        	System.out.println(num);
+        	
+        	System.out.println("오늘의 환율은 1$에 0.88\u20ac입니다.");
+        	
+        	double pi = 3.1415;
+        	int wholeNumber = (int)pi;
+        	System.out.println((int)pi);
+        	              	
+        	
+        	}
+}
