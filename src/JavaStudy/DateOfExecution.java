596cec791669a3dc078f444f5f240141a6432fcd
KIM HYUNJOONG <zzooong2@naver.com>
2023-09-03 23:47:31 +0900

    day6

---
+++ src/JavaStudy/DateOfExecution.java
@@ -0,0 +1,15 @@
+package JavaStudy;
+import java.time.LocalDate;
+
+class DateOfExecution {
+	static String date;
+	
+	static {
+		LocalDate nDate = LocalDate.now();
+		date = nDate.toString();
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(date);
+	}
+}
