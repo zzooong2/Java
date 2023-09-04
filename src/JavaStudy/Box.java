e53cf8481c29094d3cab50fae3e8223e58c28298
KIM HYUNJOONG <zzooong2@naver.com>
2023-09-04 19:36:12 +0900

    day7

---
+++ src/JavaStudy/Box.java
@@ -0,0 +1,6 @@
+package JavaStudy;
+class Box {
+ public static void main(String[] args) {
+		System.out.printf("정수는 %d,실수는 %f, 문자는%c", 12, 24.5, 'A');
+	}
+}
--- src/JavaStudy/Practice.java
+++ src/JavaStudy/Practice.java
@@ -1,11 +1,7 @@
 package JavaStudy;
 class Practice {
-	public static void main(String[] args) {
-		String num1 = "Funny";
-		String num2 = "Java";
-		String num3 = num1.concat(num2);
-		String num4 = num3.substring(3, 7);
-		System.out.println(num3);
-		System.out.println(num4);
-	}
-} 
+    public static void main(String[] args) {
+        for(int i = 1; i < 10; i++)
+        	System.out.println("5 x " + i + " = " + (5*i));
+    }
+}
