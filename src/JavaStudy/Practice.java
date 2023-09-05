c1952b996d73018a2e691ed8453bc526adb89087
KIM HYUNJOONG <zzooong2@naver.com>
2023-09-05 18:49:19 +0900

    day8

--- src/JavaStudy/Practice.java
+++ src/JavaStudy/Practice.java
@@ -1,8 +1,16 @@
 package JavaStudy;
 class Practice {
     public static void main(String[] args) {
-    	int[] ar = {1, 2, 3, 4, 5};
-    	for(int i = 0; i < ar.length; i++)
-    		System.out.println(ar[i]);
+    	String[][] ar =  {
+    	{"ㅋㅋ", "배고프다"},
+    	{"저녁", "메뉴는"},
+    	{"금득", "씨가만든카레"}
+    	};
+    	for(int i = 0; i < ar.length; i++) {
+    		for(int j = 0; j < ar[i].length; j++) {
+    			System.out.print(ar[i][j] + "\t");
+        }
+    	    System.out.println();
+    	}
     }
 }
---
+++ src/JavaStudy/Practice2.java
@@ -0,0 +1,13 @@
+package JavaStudy;
+class Practice2 {
+	public static void main(String[] args) {
+		int[] i = new int[3];
+		i[0] = 1;
+		i[1] = 2;
+		i[2] = 3;
+		
+		System.out.println(i[0]);
+		System.out.println(i[1]);
+		System.out.println(i[2]);
+	}
+}
