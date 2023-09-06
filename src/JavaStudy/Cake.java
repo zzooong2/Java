e9ac276a30c8dfc331fedd260969868b7ab928a8
KIM HYUNJOONG <zzooong2@naver.com>
2023-09-06 18:28:48 +0900

    day9

--- src/JavaStudy/Cake.java
+++ src/JavaStudy/Cake.java
@@ -1,3 +1,9 @@
 package JavaStudy;
 class Cake {
+	// Object 클래스의 toString 메소드를 오버라이딩
+	public String toString() {
+		// Object 클래스의 toString 메소드 호출하여 반환 결과 출력
+		System.out.println(super.toString());
+		return "My birthday cake";
+	}
 }
--- src/JavaStudy/CheeseCake.java
+++ src/JavaStudy/CheeseCake.java
@@ -1,3 +1,7 @@
 package JavaStudy;
 class CheeseCake extends Cake {
+	// Cake 클래스의 toString 메소드를 오버라이딩
+	public String toString() {
+		return "My birthday Cheese cake";
+	}
 }
