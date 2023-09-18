7cbd31df59f94da0c063759217756d7884888d5f
zzooong2 <143516113+zzooong2@users.noreply.github.com>
2023-09-18 18:12:05 +0900

    study

---
+++ JavaStudy/Generics2/Box.java
@@ -0,0 +1,8 @@
+package Generics2;
+
+public class Box<T> {
+	protected T ob;
+	public void set (T o) { ob = o; }
+	public T get() { return ob; }
+
+}
--- JavaStudy/Inheritance/InjuryPeriodException.java
+++ JavaStudy/Inheritance/InjuryPeriodException.java
@@ -4,5 +4,4 @@ class InjuryPeriodException extends Exception {
 	public InjuryPeriodException() {
 		super("정정합니다. 시즌아웃입니다.");
 	}
-
 }
