af3a989a40a7f61075a23a9fdf177f8e817d45c0
zzooong2 <143516113+zzooong2@users.noreply.github.com>
2023-09-29 15:02:59 +0900

    study

---
+++ JavaStudy/Date_Time/LocalDateDemo2.java
@@ -0,0 +1,25 @@
+package Date_Time;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class LocalDateDemo2 {
+	public static void main(String[] args) {
+		// 오늘
+		LocalDate today = LocalDate.now();
+		System.out.println("Today : " + today);
+		
+		// 올 해의 크리스마스
+		LocalDate christmas = LocalDate.of(today.getYear(), 12, 25);
+		System.out.println("Christmas : " + christmas);
+		
+		// 크리스마스까지 앞으로 며칠?
+		Period left = Period.between(today, christmas);
+		System.out.println("크리스마스까지 앞으로 : " + left.getMonths() + " 개월 " + left.getDays() + " 일 ");
+		
+		// 올 해의 크리스마스 이브
+		LocalDate eve = christmas.minusDays(1);
+		System.out.println("Chirstmas Eve : " + eve);
+	}
+
+}
