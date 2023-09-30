10d738cdbf0215846ff978bf72741c25d178753d
zzooong2 <143516113+zzooong2@users.noreply.github.com>
2023-09-30 14:34:36 +0900

    study

---
+++ JavaStudy/FirstJava/FirstJavaProgram.java
@@ -0,0 +1,9 @@
+package FirstJava;
+
+public class FirstJavaProgram {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java");
+		System.out.println("First Java Program");
+	}
+
+}
