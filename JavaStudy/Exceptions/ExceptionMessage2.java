9ec792132a6d5c5741b461110407feb9b89ad5ac
zzooong2 <143516113+zzooong2@users.noreply.github.com>
2023-10-05 21:51:40 +0900

    study

---
+++ JavaStudy/Exceptions/ExceptionMessage2.java
@@ -0,0 +1,19 @@
+package Exceptions;
+class ExceptionMessage2 {
+	public static void md1(int n) {
+		md2(2, 0); // 이 지점으로 md2로부터 예외가 넘어온다. 
+	}
+	public static void md2(int n1, int n2) {
+		int r = n1 / n2; // 예외 발생 지점.
+	}
+	public static void main(String[] args) {
+		try{
+			md1(3); // 이 지점에서 md1으로부터 예외가 넘어온다.
+		}
+		catch(Throwable e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("Good Bye~~~");
+	}
+} 
---
+++ JavaStudy/ExceptionsTest/Case1.java
@@ -0,0 +1,8 @@
+package ExceptionsTest;
+
+public class Case1 {
+	public static void main(String[] args) {
+		
+	}
+
+}
