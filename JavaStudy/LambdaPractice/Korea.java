60bd5ac38e964aa4d4c45deaffb8deafb6aefaef
zzooong2 <143516113+zzooong2@users.noreply.github.com>
2023-10-10 14:56:57 +0900

    study

---
+++ JavaStudy/LambdaPractice/Korea.java
@@ -0,0 +1,14 @@
+package LambdaPractice;
+
+public class Korea {
+	public static String country = "대한민국";
+	
+	public static void showString(Football f, String s) {
+		f.print(s);
+	}
+	
+	public static void main(String[] args) {
+	     showString((s) -> { System.out.println(country + s); }, " 항저우 올림픽 축구 금메달!!");
+	}
+
+}
