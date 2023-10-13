13e53235624d133dcf22990f9ed76bcdf672aea7
zzooong2 <143516113+zzooong2@users.noreply.github.com>
2023-10-13 17:29:08 +0900

    study

---
+++ JavaStudy/ExtendsPractice/Physical.java
@@ -0,0 +1,9 @@
+package ExtendsPractice;
+
+public class Physical extends Player1{
+	public Physical() {
+		System.out.println("My height is 193cm");
+		System.out.println("My weight is 90kg");
+	}
+
+}
---
+++ JavaStudy/ExtendsPractice/Player1.java
@@ -0,0 +1,9 @@
+package ExtendsPractice;
+
+public class Player1 {
+	public Player1() {
+		System.out.println("My name is Scott Mctominay");
+		System.out.println("My strong point is goal decisiveness");
+	}
+
+}
