2d1b5c2ecc68308fc515d9ab98416ad3e2300439
zzooong2 <143516113+zzooong2@users.noreply.github.com>
2023-10-14 23:44:55 +0900

    study

--- JavaStudy/ExtendsPractice/Hero.java
+++ JavaStudy/ExtendsPractice/Hero.java
@@ -2,7 +2,7 @@ package ExtendsPractice;
 
 public class Hero {
 	public static void main(String[] args) {
-		Information man = new Information("Scott Mctominay", 193, 90, "Manchester United");
+		Information man = new Information("Bruno", 193, 90, "Manchester United");
 		man.tellYourInfo();
 	}
 
