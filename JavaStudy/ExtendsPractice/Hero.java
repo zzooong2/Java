69f3ffa3530f87db0a996911e9f866c4a0b0af84
zzooong2 <zzooong2@naver.com>
2023-10-25 11:01:52 +0900

    study

--- JavaStudy/ExtendsPractice/Hero.java
+++ JavaStudy/ExtendsPractice/Hero.java
@@ -2,7 +2,7 @@ package ExtendsPractice;
 
 public class Hero {
 	public static void main(String[] args) {
-		Information man = new Information("Diogo Dalot", 180, 68, "Manchester United");
+		Information man = new Information("Harry Maguire", 193, 100, "Manchester United");
 		man.tellYourInfo();
 	}
 
