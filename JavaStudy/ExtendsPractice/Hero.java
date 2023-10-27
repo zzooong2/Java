03265defb83c1b99c255fa5ceac99f3e764a1375
zzooong2 <zzooong2@naver.com>
2023-10-27 19:53:38 +0900

    study

--- JavaStudy/ExtendsPractice/Hero.java
+++ JavaStudy/ExtendsPractice/Hero.java
@@ -2,7 +2,7 @@ package ExtendsPractice;
 
 public class Hero {
 	public static void main(String[] args) {
-		Information man = new Information("Harry Maguire", 193, 100, "Manchester United");
+		Information man = new Information("Bruno Fernandes", 180, 70, "Manchester United");
 		man.tellYourInfo();
 	}
 
