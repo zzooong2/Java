3a346bc6a8251f465b4bf31d3ff25dd4402e513e
zzooong2 <zzooong2@naver.com>
2023-10-27 19:56:06 +0900

    study

--- JavaStudy/LambdaPractice/Deffender.java
+++ JavaStudy/LambdaPractice/Deffender.java
@@ -5,7 +5,7 @@ public class Deffender {
 		String player = "RAPAHEL VARANE";
 		
 		Football fb = (s) -> { System.out.println(player + s); };
-		fb.print(" 선수, 상대 공격수를 피지컬로 제압해버립니다.");
+		fb.print("선수, 상대 공격수를 피지컬로 제압해버립니다.");
 	}
 
 }
