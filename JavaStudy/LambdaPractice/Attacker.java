50c72adf54333186bc376bee7c23db6296138de4
zzooong2 <zzooong2@naver.com>
2023-10-29 21:14:59 +0900

    study

--- JavaStudy/LambdaPractice/Attacker.java
+++ JavaStudy/LambdaPractice/Attacker.java
@@ -2,7 +2,7 @@ package LambdaPractice;
 
 public class Attacker {
 	public static void main(String[] args) {
-		String player = "RASMUS HOJLUND";
+		String player = "황희찬";
 		
 		Football fb = new ManchesterUnited();
 		fb.print(player + " 선수의 무회전 슛팅!");
