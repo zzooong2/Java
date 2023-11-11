b684db592f69a7816b309c9b9d2fcddd1cf8c836
zzooong2 <zzooong2@naver.com>
2023-11-11 11:41:43 +0900

    study

--- JavaStudy/LambdaPractice/Deffender.java
+++ JavaStudy/LambdaPractice/Deffender.java
@@ -2,7 +2,7 @@ package LambdaPractice;
 
 public class Deffender {
 	public static void main(String[] args) {
-		String player = "RAPAHEL VARANE";
+		String player = "KIM MINJAE";
 		
 		Football fb = (s) -> { System.out.println(player + s); };
 		fb.print("선수, 상대 공격수를 피지컬로 제압해버립니다.");
